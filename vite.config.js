872d10315711b43dc3bb8fd5d57933baec44a16d
Rija Faisal <rijafaisal5@gmail.com>
2025-12-10 01:47:35 +0500

    yup

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,15 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        delivery: resolve(__dirname, 'delivery.html'),
+        ride: resolve(__dirname, 'ride.html'),
+        shopping: resolve(__dirname, 'shopping.html'),
+        confirmation: resolve(__dirname, 'confirmation.html'),
+      }
+    }
+  }
 })
